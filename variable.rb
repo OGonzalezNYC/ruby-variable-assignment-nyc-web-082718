2985bf079c6da2b6a3ca773942e7973227bb3d2e
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-28 23:56:30 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -7,3 +7,12 @@ def greet(name)
 end
 
 
+archer = {
+      "name" => "Sterling Mallory Archer",
+      "co-workers"=> ["Lana Kane", "Cyril Figgis", "Cheryl Tunt", "Pam Poovey", "Dr Krieger"],
+      "favorite_drink" => "Bloody Mary",
+      "Quotes" => ["I swear to god, I had something for this", "Phrasing", "Boop", "Danger Zone", "Read a book", "Do you not?", "Can't or won't?"]
+  }
+  
+
+    archer["Quotes"].sample
